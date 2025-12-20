8e21587168712c26e9c8cd9cbe09a874ebf1efca
Malhar A. Patel <malharalpeshpatel@gmail.com>
2025-12-20 19:54:28 +0000

    Refactor `GameState` to have `h : 2 ≤ n`; Change `row`, `col` in `Point` to have type `Nat`; Add functions `get`, `getNeighborPoints` and `playAt`

--- GoLean/Go.lean
+++ GoLean/Go.lean
@@ -5,35 +5,41 @@ Authors : Malhar A. Patel
 import Lean
 import Mathlib
 
-inductive Color
-  | B
-  | W
-  deriving DecidableEq, Repr, Inhabited
-
-def printColor : Color → String
-  | .B => "Black"
-  | .W => "White"
-
 inductive Stone
   | B
   | W
   deriving DecidableEq, Repr, Inhabited
 
-structure GameState (n : Nat) where
+def printStone : Stone → String
+  | .B => "Black"
+  | .W => "White"
+
+def nextTurn : Stone → Stone
+  | .B => .W
+  | .W => .B
+
+structure GameState (n : Nat) (h : 2 ≤ n := by decide) where
   board : Vector (Vector (Option Stone) n) n
-  toMove: Color
+  toMove : Stone
   prev_board : Option <| Vector (Vector (Option Stone) n) n
-  deriving DecidableEq
+
+structure Point (n : Nat) where
+  mk ::
+  row : Nat
+  col : Nat
+  row_lt_n : row < n := by decide
+  col_lt_n : col < n := by decide
+  deriving DecidableEq, Repr
 
 def emptyBoard (n : Nat) : Vector (Vector (Option Stone) n) n :=
   let r : Vector (Option Stone) n :=
     ⟨Array.replicate n none, by simp⟩
   ⟨Array.replicate n r, by simp⟩
 
-instance {n} : Inhabited (GameState n) where
+instance {n} {h : 2 ≤ n} : Inhabited (GameState n h) where
   default := {
     board := emptyBoard n,
-    toMove := Color.B,
+    toMove := Stone.B,
     prev_board := none
     }
 
@@ -41,8 +47,42 @@ instance {n} : Inhabited (GameState n) where
 
 #eval (Inhabited.default : GameState 19)
 
-def get {n} (gsn : GameState n) (r c : Fin n) : Option Stone :=
+def dflt : GameState 19 :=
+  Inhabited.default
+
+def get_old {n} (h : 2 ≤ n := by decide) (gsn : GameState n h)
+    (r c : Fin n) : Option Stone :=
   (gsn.board[r]'(by simp))[c]'(by simp)
+
+#eval get_old _ (Inhabited.default : GameState 19) 1 1
+
+def getOptStone {n} (h : 2 ≤ n := by decide) (gsn : GameState n h) (p : Point n)
+    : Option Stone :=
+  (gsn.board[p.row]'(p.row_lt_n))[p.col]'(p.col_lt_n)
+
+def getNeighborPoints {n} (h : 2 ≤ n := by decide) (p : Point n)
+    : Vector (Option <| Point n) 4 :=
+  let up : Option <| Point n :=
+    if p.row == 0 then none
+    else some ⟨p.row - 1, p.col, by grind [p.row_lt_n], p.col_lt_n⟩
+  let down : Option <| Point n :=
+    if row_eq_n : p.row == n - 1 then none
+    else some ⟨p.row + 1, p.col, by grind [p.row_lt_n], p.col_lt_n⟩
+  let left : Option <| Point n :=
+    if p.col == 0 then none
+    else some ⟨p.row, p.col - 1, p.row_lt_n, by grind[p.col_lt_n]⟩
+  let right : Option <| Point n :=
+    if row_eq_n : p.col == n - 1 then none
+    else some ⟨p.row, p.col + 1, p.row_lt_n, by grind[p.col_lt_n]⟩
+  ⟨#[up, down, left, right], by simp⟩
+
+def playAt {n} {h : 2 ≤ n} (gsn : GameState n h) (p : Point n)
+    : GameState n h :=
+  let board := gsn.board
+  let row := board[p.row]'(p.row_lt_n)
+  let modified_row := row.set p.col (some gsn.toMove) (p.col_lt_n)
+  let modified_board := board.set p.row modified_row p.row_lt_n
+  ⟨modified_board, nextTurn gsn.toMove, some board⟩
 
 def printOptStone : Option Stone → String
 | some .B => "●"
@@ -55,16 +95,17 @@ def printRow {n} (vec : Vector (Option Stone) n) : String :=
 def printBoard {n} (bvec : Vector (Vector (Option Stone) n) n) : String :=
   String.intercalate "\n" (bvec.map printRow).toList
 
-instance {n} : Repr <| GameState n where
+instance {n} {h : 2 ≤ n} : Repr <| GameState n h where
   reprPrec gsn _ :=
-    let h := "To Move: " ++ printColor gsn.toMove ++ "\n"
-    let b := printBoard gsn.board
-    (h ++ b).toFormat
+    let header := "To Move: " ++ printStone gsn.toMove ++ "\n"
+    let brd := printBoard gsn.board
+    (header ++ brd).toFormat
 
 #eval (Inhabited.default : GameState 19)
 
+#eval dflt
 
-
+#eval playAt dflt (.mk 1 0)
 
 
 
