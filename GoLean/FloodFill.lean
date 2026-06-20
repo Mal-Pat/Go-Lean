7dd6c908a010053721861c477f98117204289948
Malhar A. Patel <malharalpeshpatel@gmail.com>
2026-06-20 21:01:31 +0000

    Create `GoLean/FloodFill.lean`

---
+++ GoLean/FloodFill.lean
@@ -0,0 +1,44 @@
+import GoLean.Basic
+
+/-- Delete duplicates in a list -/
+def deldups {α} [DecidableEq α] (l : List α) : List α :=
+  l.foldl (fun unql a => if a ∈ unql then unql else a :: unql) []
+
+/-- Helper for floodfill algorithm -/
+def floodFillHelper {s} (board : Board s) (queue : PointsList s)
+  (visited : PointsList s) (liberties : PointsList s) (empty : Bool)
+  (fuel : Nat)
+    : (PointsList s) × (PointsList s) :=
+  match fuel with
+  | 0 => (visited, deldups liberties)
+  | f + 1 => match queue with
+    | [] => (visited, deldups liberties)
+    | cur :: rest =>
+      if cur ∈ visited then
+        floodFillHelper board rest visited liberties empty f
+      else
+        let (same, diff) := cur.getNbhdPoints.partition
+          fun p => (board.getColorAt? cur == board.getColorAt? p)
+        let libs :=
+          if empty then diff
+          else diff.filter fun p => (board.getColorAt? p == none)
+        floodFillHelper board (rest ++ same.toList) (cur :: visited) (liberties ++ libs.toList) empty f
+
+/-- The floodfill algorithm -/
+def floodFill {s} (board : Board s) (point : Point s) (empty : Bool)
+    : (PointsList s) × (PointsList s) :=
+  let fuel := (s.rl * s.cl) + 1
+  floodFillHelper board [point] [] [] empty fuel
+
+/-- Get the StoneGroup at point `p` on `board` -/
+def Board.getGroupAt {s} (board : Board s) (p : Point s)
+    : Group s :=
+  let empty := board.isEmptyPoint p
+  let (points, liberties) := floodFill board p empty
+  -- maybe add check for StoneGroup here
+  ⟨board.getStoneAt p, points.toArray, liberties.toArray⟩
+
+/-- Get all StoneGroups from array of points -/
+def Board.getStoneGroupsAt {s} (board : Board s) (parr : PointsArr s)
+    : GroupsArr s :=
+  parr.map fun point => board.getGroupAt point
