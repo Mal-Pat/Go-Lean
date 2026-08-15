8bf3adab19d829e928e40ad48023a11f8361f4aa
Malhar A. Patel <malharalpeshpatel@gmail.com>
2026-08-15 15:52:55 +0100

    Import all files in `GoLean.lean`

--- GoLean.lean
+++ GoLean.lean
@@ -1,1 +1,8 @@
-import GoLean.Basic
+import GoLean.Core.Types
+import GoLean.Core.Board
+import GoLean.Core.Rules
+import GoLean.Core.Play
+import GoLean.Core.Score
+import GoLean.Core.Game
+import GoLean.Protocol
+import GoLean.Widget
